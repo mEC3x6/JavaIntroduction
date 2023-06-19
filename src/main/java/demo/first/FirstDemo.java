3c9b6bfd07f34cbf466c70191cbaeb430001e9d3
mEC3x6 <pkirilov90@gmail.com>
2023-06-19 20:57:22 +0300

    added wor with console

---
+++ src/main/java/demo/first/FirstDemo.java
@@ -0,0 +1,19 @@
+package demo.first;
+
+import java.util.Scanner;
+
+//defition of class
+public class FirstDemo {
+    public static void main (String [] args){
+        //method
+        System.out.println("Hello JAVA");
+        System.out.println("Please enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String  name =  scanner.nextLine();
+        System.out.println("How old are you");
+        int age = scanner.nextInt();
+        System.out.println("Nice to meet you, " + name + ". Your age is: " + age);
+
+
+    }
+}
