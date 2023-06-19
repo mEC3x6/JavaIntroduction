1cc9c5b4367c66abfcac3ee9defc9a51fa819f74
mEC3x6 <pkirilov90@gmail.com>
2023-06-19 21:43:53 +0300

    First Homework

--- src/main/java/demo/first/FirstHomework.java
+++ src/main/java/demo/first/FirstHomework.java
@@ -4,15 +4,12 @@ import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class FirstHomework {
-    public static void main(String [] arg){
-        System.out.println("Please enter your name:");
+    public static void main(String [] arg) {
+        System.out.println("What is your name?");
         Scanner scanner = new Scanner(System.in);
-        String  name =  scanner.nextLine();
-        System.out.println("What is yor favourite hobby");
+        String name = scanner.nextLine();
+        System.out.println("What is your favourite hobby?");
         String hobby = scanner.nextLine();
         System.out.println("Hello " + name + ", your favourite hobby is " + hobby + "!");
-
-
     }
-
 }
